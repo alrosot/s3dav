4f11ec5db44d6453fe003c7268b83ea5f3633af4
pcarion <pcarion@28f3d6e3-b010-0410-b113-e33a592e2231>
2006-04-09 02:25:30 +0000

    add logs in files

---
+++ trunk/src/org/carion/s3dav/webdav/htmlPages/LogsPage.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2006, Pierre Carion.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.carion.s3dav.webdav.htmlPages;
+
+class LogsPage extends Page {
+    LogsPage(String pageName) {
+        super("Logs Page", pageName);
+    }
+
+    boolean needsRepository() {
+        return false;
+    }
+
+    void page() {
+        _w.article("Logs");
+        _w.out("<code><pre>");
+        _w.out(_repository.getRawLogs());
+        _w.out("</pre></code>");
+        _w.article_end();
+    }
+}
